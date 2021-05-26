366079e94b9164751782fac5260d98ede722fe62
Persian Prince <persianpros@yahoo.com>
2021-05-26 13:34:08 +0430

    Update package.mk

--- package.mk
+++ package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="rpi-e2-procfs"
-PKG_VERSION="5.10.36"
+PKG_VERSION="5.10.39"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
