2f2a2574e9221df881b794f95e734d5713d28fe1
AYP <persianpros@yahoo.com>
2022-10-14 09:05:07 +0330

    Update package.mk

--- package.mk
+++ package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="rpi-e2-procfs"
-PKG_VERSION="5.15.70"
+PKG_VERSION="5.15.73"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
